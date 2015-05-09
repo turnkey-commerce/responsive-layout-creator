1db20041cde03e29a76f622fa7b73825a452ca24
James Culbertson <info@turnkey-commerce.com>
2015-05-09 17:13:08 -0500

    Wire up event handling and add reading of list elements.

---
+++ constants.py
@@ -0,0 +1,9 @@
+'''
+Created on May 9, 2015
+
+@author: James Culbertson
+'''
+
+import os
+
+AVAILABLE_LAYOUT_ELEMENTS_FILE = os.path.join("resources","available_layouts.json")
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
-from PySide import QtGui, QtCore
+from PySide import QtGui
 from main_ui import Ui_MainWindow
+import constants as c
+from PySide.QtGui import QStandardItemModel
 
 class MainWindow(QtGui.QMainWindow):
 
@@ -12,7 +14,53 @@ class MainWindow(QtGui.QMainWindow):
         self.show()
 
     def init_ui(self):
-        pass
+        #Menu Events
+        self.ui.menuLoadLayoutTemplate.activated.connect(self.menuLoadLayoutTemplate_activated)
+        self.ui.menuSaveLayoutTemplate.activated.connect(self.menuSaveLayoutTemplate_activated)
+        self.ui.menuSaveHtmlFile.activated.connect(self.menuSaveHtmlFile_activated)
+        #Button Events
+        self.ui.buttonAdd.clicked.connect(self.buttonAdd_clicked)
+        self.ui.buttonMoveDown.clicked.connect(self.buttonMoveDown_clicked)
+        self.ui.buttonMoveUp.clicked.connect(self.buttonMoveUp_clicked)
+        self.ui.buttonRemoveSelected.clicked.connect(self.buttonRemoveSelected_clicked)
+        self.ui.buttonSaveHtmlFile.clicked.connect(self.buttonSaveHtmlFile_clicked)
+        #Get the initial Available List Items
+        self._get_available_layout_elements()
+  
+    def menuLoadLayoutTemplate_activated(self):
+        self.load_template()
+        
+    def menuSaveLayoutTemplate_activated(self):
+        self.save_template()
+        
+    def menuSaveHtmlFile_activated(self):
+        self.save_html_file()
+        
+    def buttonAdd_clicked(self):
+        self.add_selected_item()
+        
+    def buttonMoveDown_clicked(self):
+        self.move_selected_item_down()
+    
+    def buttonMoveUp_clicked(self):
+        self.move_selected_item_up()
+        
+    def buttonRemoveSelected_clicked(self):
+        self.remove_selected_item()
+        
+    def buttonSaveHtmlFile_clicked(self):
+        self.save_html_file()
+        
+    def _get_available_layout_elements(self):
+        import json
+        file_open = open(c.AVAILABLE_LAYOUT_ELEMENTS_FILE, 'r')
+        file_contents = file_open.read()
+        layout_elements  = json.loads(file_contents)
+        model = QStandardItemModel(self.ui.listViewAvailable)
+        for layout_element in layout_elements:
+            item = QtGui.QStandardItem(layout_element)
+            model.appendRow(item)
+        self.ui.listViewAvailable.setModel(model)
         
 
 def main():
