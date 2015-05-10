bd563db9653e4a53479c1b33f85cc0f9120a42ea
James Culbertson <info@turnkey-commerce.com>
2015-05-10 13:09:44 -0500

    Implement getting and saving content from snippets.

--- constants.py
+++ constants.py
@@ -6,4 +6,8 @@ Created on May 9, 2015
 
 import os
 
-AVAILABLE_LAYOUT_ELEMENTS_FILE = os.path.join("resources","available_layouts.json")
+RESOURCES_FOLDER = "resources"
+
+AVAILABLE_LAYOUT_ELEMENTS_FILE = os.path.join(RESOURCES_FOLDER,"available_layouts.json")
+AVAILABLE_LAYOUT_ITEM_NAME = "name"
+AVAILABLE_LAYOUT_ITEM_SNIPPET_FILE = "snippetFile"
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import sys
+import sys, os
 from PySide import QtGui
 from main_ui import Ui_MainWindow
 import constants as c
@@ -9,6 +9,7 @@ class MainWindow(QtGui.QMainWindow):
         super(MainWindow, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        self.available_layout_elements = []
         self.init_ui()
         self.show()
 
@@ -62,7 +63,7 @@ class MainWindow(QtGui.QMainWindow):
             self.ui.listWidgetSelected.setCurrentRow(current_index - 1)
         elif current_index:
             self.ui.listWidgetSelected.setCurrentRow(current_index)
-        
+
     def move_selected_item_down(self):
         current_index = None
         for item in self.ui.listWidgetSelected.selectedItems():
@@ -73,26 +74,76 @@ class MainWindow(QtGui.QMainWindow):
             self.ui.listWidgetSelected.setCurrentRow(current_index + 1)
         elif current_index:
             self.ui.listWidgetSelected.setCurrentRow(current_index)
-        
+
     def add_selected_item(self):
         current_item = self.ui.listWidgetAvailable.currentItem()
-        self.ui.listWidgetSelected.addItem(current_item.text())
-            
+        if current_item is not None:
+            self.ui.listWidgetSelected.addItem(current_item.text())
+
     def remove_selected_item(self):
         for item in self.ui.listWidgetSelected.selectedItems():
             self.ui.listWidgetSelected.takeItem(self.ui.listWidgetSelected.row(item))
         #Remove selection to prevent accidents with multiple clicks.
         for index in range(self.ui.listWidgetSelected.count()):
             self.ui.listWidgetSelected.item(index).setSelected(False)
+
+    def save_html_file(self):
+        # Assemble the content from the snippet file list from the selected elements.
+        fileName = QtGui.QFileDialog.getSaveFileName(self, "Save HTML File", "", "HTML Files (*.htm *.html)")
+        if fileName[0] is not None and fileName[0] is not u'':
+            snippet_files = self._get_snippet_file_list()
+            content = self._get_content_from_snippet_files(snippet_files)
+            # Check if file exists and if so back it up to .bak
+            if os.path.exists(fileName[0]):
+                import shutil
+                shutil.copyfile(fileName[0], fileName[0] + ".bak")
+            file_save = open(fileName[0], 'w')
+            file_save.write(content)
+   
+    def _get_snippet_file_list(self):
+        snippet_file_list = []
+        #always add a header snippet that will contain boilerplate for the header.
+        snippet_file_list.append(os.path.join(c.RESOURCES_FOLDER, "header_snippet.txt"))
+        for index in range(self.ui.listWidgetSelected.count()):
+            element_name = self.ui.listWidgetSelected.item(index).text()
+            index = self._find(self.available_layout_elements, c.AVAILABLE_LAYOUT_ITEM_NAME, element_name)
+            snippet_name = self.available_layout_elements[index][c.AVAILABLE_LAYOUT_ITEM_SNIPPET_FILE]
+            snippet_file_list.append(os.path.join(c.RESOURCES_FOLDER, snippet_name))
+        #always add a footer snippet that will contain boilerplate for the footer.
+        snippet_file_list.append(os.path.join(c.RESOURCES_FOLDER, "footer_snippet.txt"))
+        return snippet_file_list
+    
+    def _get_content_from_snippet_files(self, snippet_files):
+        content = ""
+        for snippet_file in snippet_files:
+            try:
+                file_open = open(snippet_file, 'r')
+                file_contents = file_open.read()
+                content += file_contents + os.linesep
+            except Exception as exc:
+                self._show_message_box('Problem reading Snippet file: ' + str(exc).replace("\\\\", "\\"))
+                return ""
+        return content
         
     def _get_available_layout_elements(self):
         import json
         file_open = open(c.AVAILABLE_LAYOUT_ELEMENTS_FILE, 'r')
         file_contents = file_open.read()
-        layout_elements  = json.loads(file_contents)
-        for layout_element in layout_elements:
-            self.ui.listWidgetAvailable.addItem(layout_element)
-                   
+        self.available_layout_elements  = json.loads(file_contents)
+        for layout_element in self.available_layout_elements:
+            self.ui.listWidgetAvailable.addItem(layout_element[c.AVAILABLE_LAYOUT_ITEM_NAME])
+            
+    def _find(self, lst, key, value):
+        for i, dic in enumerate(lst):
+            if dic[key] == value:
+                return i
+        return ValueError("Key not found")
+
+    def _show_message_box(self, message):
+        messageBox = QtGui.QMessageBox()
+        messageBox.setText(message)
+        messageBox.exec_()
+
 
 def main():
     app = QtGui.QApplication(sys.argv)
